ebf2a92b5be755ed2ccb874b0a4b7ee4fa3e5cca
Vivi <70662490+VivianeSouza923@users.noreply.github.com>
2023-09-26 12:56:52 -0300

    Add files via upload
    add questão1 da lista de java da cadeira de POO

---
+++ question1.java
@@ -0,0 +1,8 @@
+// 1. Escreva um programa que imprima os nÃºmeros de 1 a 10 usando um loop `for`.
+public class MyClass {
+    public static void main(String args[]) {
+      for(int i=1; i<11; i++){
+          System.out.println(i);
+      }
+    }
+}
